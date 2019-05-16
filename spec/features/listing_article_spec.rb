842216ff126fe262068c925816e0a7ff80f5247e
Josy <josyanjos.souza@gmail.com>
2019-05-16 10:27:11 -0300

    Show new article button to signed un user

--- spec/features/listing_article_spec.rb
+++ spec/features/listing_article_spec.rb
@@ -1,15 +1,15 @@
 require "rails_helper"
 
-RSpec.feature "Listing Articles" do 
+RSpec.feature "Listing Articles" do
 
-  before do 
-    john = User.create(email: "john@exemple.com", password: "password")
-    @article1 = Article.create(title: "The first article", body: "Lorem ipsum dolor sit amet, consectetur", user: john)
-    @article2 = Article.create(title: "The second article", body: "Body of 2nd article", user: john)
+  before do
+    @john = User.create(email: "john@exemple.com", password: "password")
+    @article1 = Article.create(title: "The first article", body: "Lorem ipsum dolor sit amet, consectetur", user: @john)
+    @article2 = Article.create(title: "The second article", body: "Body of 2nd article", user: @john)
 
   end
-  
-  scenario "A user lists all articles" do 
+
+  scenario "A user lists all articles" do
     visit "/"
 
     expect(page).to have_content(@article1.title)
@@ -18,7 +18,33 @@ RSpec.feature "Listing Articles" do
     expect(page).to have_content(@article2.body)
     expect(page).to have_link(@article1.title)
     expect(page).to have_link(@article2.title)
-  end	
+  end
+
+  scenario "with articles created user not signed in" do
+    visit "/"
+
+    expect(page).to have_content(@article1.title)
+    expect(page).to have_content(@article1.body)
+    expect(page).to have_content(@article2.title)
+    expect(page).to have_content(@article2.body)
+    expect(page).to have_link(@article1.title)
+    expect(page).to have_link(@article2.title)
+    expect(page).not_to have_link("New Article")
+  end
+
+  scenario "with articles created user signed in" do
+    login_as(@john)
+    visit "/"
+
+    expect(page).to have_content(@article1.title)
+    expect(page).to have_content(@article1.body)
+    expect(page).to have_content(@article2.title)
+    expect(page).to have_content(@article2.body)
+    expect(page).to have_link(@article1.title)
+    expect(page).to have_link(@article2.title)
+    expect(page).to have_link("New Article")
+  end
+
 
   scenario "A user has no articles" do
     Article.delete_all
@@ -34,6 +60,6 @@ RSpec.feature "Listing Articles" do
 
     within("h1#no-articles") do
       expect(page).to have_content("No Articles Created")
-     end 
-  end 
+     end
+  end
 end
